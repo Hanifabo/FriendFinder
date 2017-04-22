8af1850187b718cb86c5e220a551db7e60bf9223
hanifabo <hanifabo@gmail.com>
2017-04-22 08:55:28 -0700

    fre

--- server.js
+++ server.js
@@ -4,3 +4,18 @@
 var express = require('express');
 var bodyparser = require('body-parser');
 var path = require('path');
+
+var app = express();
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', function (req, res){
+    console.log(" we are here to learn");
+    res.sendFile(path.join(__dirname,'./app/public/index.html'));
+});
+
+app.listen(3000, function (err) {
+    if(err) return console.log("Server couldn't be established 'FAILED' ");
+    console.log(" Server is succcessfully up and runing ");
+})
